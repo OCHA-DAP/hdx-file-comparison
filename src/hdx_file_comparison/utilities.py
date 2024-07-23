c2c6c1f0dba4fe93150c6a7a64cc1c32f88fa4c6
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-07-23 16:47:20 +0100

    HDX-10021 Add cell level change detection

--- src/hdx_file_comparison/utilities.py
+++ src/hdx_file_comparison/utilities.py
@@ -1,12 +1,71 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+import csv
 import difflib
+import re
 
 
-def difflib_compare(filepath_1: str, filepath_2: str, encoding: str = "utf-8") -> list[str]:
+def difflib_compare(filepath_1: str, filepath_2: str, encoding: str = "utf-8") -> list[tuple]:
     diff = difflib.ndiff(
         open(filepath_1, encoding=encoding).read().splitlines(),
         open(filepath_2, encoding=encoding).read().splitlines(),
     )
-    return [x for x in diff if x[0] in ["-", "+", "?"]]
+    return [(i, x) for i, x in enumerate(diff) if x[0] in ["-", "+", "?"]]
+
+
+def detect_cell_change_from_diff(header: list[str], diff: list[tuple]):
+    cell_changes = []
+    for i in range(0, len(diff) - 1):
+        if diff[i][1].startswith("- ") and diff[i + 1][1].startswith("? "):
+            print(diff[i], flush=True)
+            print(diff[i + 1], flush=True)
+            print(diff[i + 2], flush=True)
+            print(diff[i + 3], flush=True)
+
+            # Find index of carets
+
+            idxs = [m.start() for m in re.finditer("\^", diff[i + 1][1])]
+            print(idxs, flush=True)
+            probe_row = list(diff[i][1])
+            for idx in idxs:
+                probe_row[idx] = "^"
+            probe_row = "".join(probe_row)
+
+            row = list(csv.reader([probe_row[2:]]))
+
+            for j, column in enumerate(row[0]):
+                if "^" in column:
+                    original_row = list(csv.reader([diff[i][1][2:]]))[0]
+                    new_row = list(csv.reader([diff[i + 2][1][2:]]))[0]
+                    print(j, flush=True)
+                    print(header, flush=True)
+                    print(original_row, flush=True)
+                    print(new_row, flush=True)
+                    status = (
+                        f"Column '{header[j]}' has been changed from "
+                        f"'{original_row[j]}' to '{new_row[j]}' on row '{diff[i][0]}'"
+                    )
+                    print(status, flush=True)
+                    cell_changes.append(status)
+
+            # for row in rows:
+            #     print(row, flush=True)
+
+    return cell_changes
+
+
+def process(filepath_1: str, filepath_2: str, encoding: str = "utf-8"):
+    # Get headers
+    headers = []
+    with open(filepath_2, encoding=encoding) as file_handle:
+        csv_reader = csv.reader(file_handle)
+
+        headers = next(csv_reader)
+    # Get diff
+    diff = difflib_compare(filepath_1, filepath_2, encoding="utf-8")
+
+    # Process diff
+    cell_changes = detect_cell_change_from_diff(headers, diff)
+
+    return cell_changes
--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -3,7 +3,7 @@
 
 import os
 import time
-from hdx_file_comparison.utilities import difflib_compare
+from hdx_file_comparison.utilities import difflib_compare, process
 
 
 def test_difflib_compare_small():
@@ -39,7 +39,22 @@ def test_difflib_compare_big():
     print(f"Diff took {time.time()-t0:0.3f} seconds", flush=True)
 
     for row in diff:
-        if row.startswith("?"):
+        if row[1].startswith("?"):
             print(f"'{row}'", flush=True)
 
+    assert False
+
+
+def test_process():
+    filepath_1 = os.path.join(
+        os.path.dirname(__file__), "fixtures", "2024-07-21-wfp_food_prices_afg_qc.csv"
+    )
+    # Manually removed lines from start and end, and mutated a value
+    filepath_2 = os.path.join(
+        os.path.dirname(__file__), "fixtures", "2024-07-21-wfp_food_prices_afg_qc_changed.csv"
+    )
+
+    cell_changes = process(filepath_1, filepath_2)
+
+    assert cell_changes == ["Column 'usdprice' has been changed from '0.3011' to '0.3000'"]
     assert False
