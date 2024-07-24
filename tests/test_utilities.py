3aa85f1439015735878526969bdd39c0292c06ea
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-07-24 09:00:57 +0100

    HDX-10021 update tests to use real past versions of the resources

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -6,14 +6,10 @@ import time
 from hdx_file_comparison.utilities import difflib_compare, process, compute_diff_metrics
 
 FIXTURES_DIRECTORY = os.path.join(os.path.dirname(__file__), "fixtures")
-SMALL_FILE_ORIGINAL = os.path.join(FIXTURES_DIRECTORY, "2024-07-21-wfp_food_prices_afg_qc.csv")
-# Manually removed lines from start and end, and mutated a value
-SMALL_FILE_CHANGED = os.path.join(
-    FIXTURES_DIRECTORY, "2024-07-21-wfp_food_prices_afg_qc_changed.csv"
-)
-BIG_FILE_ORIGINAL = os.path.join(FIXTURES_DIRECTORY, "2024-07-21-wfp_food_prices_afg.csv")
-# Manually removed lines from start and end, and mutated a value
-BIG_FILE_CHANGED = os.path.join(FIXTURES_DIRECTORY, "2024-07-21-wfp_food_prices_afg_changed.csv")
+SMALL_FILE_ORIGINAL = os.path.join(FIXTURES_DIRECTORY, "2024-05-12-wfp_food_prices_afg_qc.csv")
+SMALL_FILE_CHANGED = os.path.join(FIXTURES_DIRECTORY, "2024-07-21-wfp_food_prices_afg_qc.csv")
+BIG_FILE_ORIGINAL = os.path.join(FIXTURES_DIRECTORY, "2024-07-14-wfp_food_prices_afg.csv")
+BIG_FILE_CHANGED = os.path.join(FIXTURES_DIRECTORY, "2024-07-21-wfp_food_prices_afg.csv")
 
 
 def test_difflib_compare_small():
@@ -22,12 +18,13 @@ def test_difflib_compare_small():
     print(f"Diff took {time.time()-t0:0.3f} seconds", flush=True)
 
     diff_metrics = compute_diff_metrics(diff)
+    print(diff_metrics, flush=True)
     for row in diff:
         print(row, flush=True)
 
-    assert diff_metrics["n_lines_changed"] == 1
-    assert diff_metrics["n_lines_removed"] == 9
-    assert diff_metrics["n_lines_added"] == 0
+    assert diff_metrics["n_lines_changed"] == 2
+    assert diff_metrics["n_lines_removed"] == 0
+    assert diff_metrics["n_lines_added"] == 4
 
 
 def test_difflib_compare_big():
@@ -36,12 +33,13 @@ def test_difflib_compare_big():
     print(f"Diff took {time.time()-t0:0.3f} seconds", flush=True)
 
     diff_metrics = compute_diff_metrics(diff)
-    for row in diff:
-        print(row, flush=True)
+    print(diff_metrics, flush=True)
+    # for row in diff:
+    #     print(row, flush=True)
 
-    assert diff_metrics["n_lines_changed"] == 1
-    assert diff_metrics["n_lines_removed"] == 10
-    assert diff_metrics["n_lines_added"] == 0
+    assert diff_metrics["n_lines_changed"] == 473
+    assert diff_metrics["n_lines_removed"] == 0
+    assert diff_metrics["n_lines_added"] == 1.0
 
 
 def test_process():
@@ -49,6 +47,8 @@ def test_process():
     cell_changes = process(SMALL_FILE_ORIGINAL, SMALL_FILE_CHANGED)
     print(f"Process took {time.time()-t0:0.3f} seconds", flush=True)
 
+    print(cell_changes, flush=True)
     assert cell_changes == [
-        "Column 'usdprice' has been changed from '0.3011' to '0.3000' on row '327'"
+        "Column 'usdprice' has been changed from '0.3606' to '0.331' on row '291'",
+        "Column 'date' has been changed from '2024-05-15' to '2024-06-15' on row '589'",
     ]
